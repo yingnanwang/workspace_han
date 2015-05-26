86a0cfb9050002c2cda28c88822e6ce5ea9c74eb
njuHan <121220135@smail.nju.edu.cn>
2015-05-26 11:06:44 +0800

    添加登录首页界面

--- yingnanwang/src/java/model/DataBase.java
+++ yingnanwang/src/java/model/DataBase.java
@@ -8,6 +8,7 @@ public class DataBase {
 		users.put("李晓彤", "121212");
 		users.put("刘金辉", "123456");
 		users.put("James", "666666");
+                users.put("han", "123");
 	}
 	public static Map<String, String> getUsers() {
 		return users;
